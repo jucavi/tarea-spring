d87b0234bab567b054aa6b5791bb1ef754d1904d
Juan Carlos Vilarrubia del Castillo <jcvilarrubia@soincon.es>
2024-04-03 08:20:54 +0200

    remove composite key SigningKey

--- src/main/java/com/example/tareaspring/repositories/SigningRepository.java
+++ src/main/java/com/example/tareaspring/repositories/SigningRepository.java
@@ -1,10 +1,9 @@
 package com.example.tareaspring.repositories;
 
 import com.example.tareaspring.models.Signing;
-import com.example.tareaspring.models.SigningKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SigningRepository extends JpaRepository<Signing, SigningKey> {
+public interface SigningRepository extends JpaRepository<Signing, Long> {
 }
