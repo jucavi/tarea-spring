0fa7388ccd8debc6ab316db00465768889e2da79
jucavi <jcarvi@gmail.com>
2024-04-05 13:21:12 +0200

    add custom date validator CustomDateConstraint

--- src/main/java/com/example/tareaspring/utils/validators/date/CustomDateConstraint.java
+++ src/main/java/com/example/tareaspring/utils/validators/date/CustomDateConstraint.java
@@ -1,2 +1,15 @@
-package com.example.tareaspring.utils.validators.date;public class CustomDateConstraint {
+package com.example.tareaspring.utils.validators.date;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = CustomDateValidator.class)
+@Target( { ElementType.METHOD, ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CustomDateConstraint {
+    String message() default "Invalid date format";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
--- src/main/java/com/example/tareaspring/utils/validators/date/CustomDateValidator.java
+++ src/main/java/com/example/tareaspring/utils/validators/date/CustomDateValidator.java
@@ -1,2 +1,30 @@
-package com.example.tareaspring.utils.validators.date;public class CustomDateValidator {
+package com.example.tareaspring.utils.validators.date;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+
+public class CustomDateValidator implements ConstraintValidator<CustomDateConstraint, LocalDate> {
+
+    private static final String DATE_PATTERN = "yyyy-MM-dd";
+
+    @Override
+    public boolean isValid(LocalDate customDateField,
+                           ConstraintValidatorContext cxt) {
+        SimpleDateFormat o = new SimpleDateFormat(DATE_PATTERN);
+        try {
+            o.setLenient(false);
+            Date d = o.parse(customDateField.toString());
+            return true;
+
+        } catch (ParseException e) {
+            //
+        }
+
+        return false;
+    }
+
 }
