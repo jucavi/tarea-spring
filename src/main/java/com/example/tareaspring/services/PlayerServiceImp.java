cfe42e5a9570e1783dcebe228957e50b85a2e6e3
jucavi <jcarvi@gmail.com>
2024-04-05 09:19:02 +0200

    exclude log files for git

---
+++ src/main/java/com/example/tareaspring/services/PlayerServiceImp.java
@@ -0,0 +1,144 @@
+package com.example.tareaspring.services;
+
+import com.example.tareaspring.csv.SigningCSV;
+import com.example.tareaspring.models.Player;
+import com.example.tareaspring.csv.PlayerCSV;
+import com.example.tareaspring.models.Signing;
+import com.example.tareaspring.repositories.PlayerRepository;
+import com.opencsv.bean.BeanVerifier;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.exceptions.CsvConstraintViolationException;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+// TODO: Move to Lombok @log annotation
+@Log4j2
+@Service
+public class PlayerService {
+
+    private final PlayerRepository repository;
+
+    public PlayerService(PlayerRepository repository) {
+        this.repository = repository;
+    }
+
+
+    // TODO: Refactorize
+    public List<Player> parseCSVFileToPlayers(@NonNull MultipartFile file) {
+
+        List<Player> players = new ArrayList<>();
+
+        // validamos que el fichero existe
+        if (file.isEmpty()) {
+            log.warn("Not CSV file present to upload");
+            return players;
+        }
+
+        // parsemos el fichero CSV a una lista de objetos Player
+        try (Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+            CsvToBean<PlayerCSV> csvToBean = new CsvToBeanBuilder(reader)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .withType(PlayerCSV.class)
+                    .withVerifier(new BeanVerifier() {
+                        @Override
+                        public boolean verifyBean(Object bean) throws CsvConstraintViolationException {
+                            PlayerCSV b = (PlayerCSV) bean;
+                            // Todas propiedades nulas
+                            if (b == null) {
+                                return false;
+                            }
+                            return true;
+                        }
+                    })
+                    .build();
+
+            List<PlayerCSV> playersObj = csvToBean.parse();
+
+            for (PlayerCSV o : playersObj) {
+                Player player = o.toBeanWithId();
+                try {
+                    repository.save(player);
+                    players.add(player);
+
+                } catch (Exception ex) {
+                    log.error("{} can't be saved: {}", player, ex.getMessage());
+                }
+            }
+
+        } catch (Exception ex) {
+            log.error("An error occurred while processing the CSV file.");
+        }
+
+        return players;
+    }
+
+    public List<Player> findAll() {
+        log.info("Retrieving all Teams from database");
+        return repository.findAll();
+    }
+
+    public Player findById(Long id) {
+        Optional<Player> playerOpt = repository.findById(id);
+
+        if (playerOpt.isPresent()) {
+            log.info("Retrieving Player with ID: {}", id);
+            return playerOpt.get();
+        }
+        log.warn("Retrieving Player with wrong ID: {}", id);
+        return null;
+    }
+
+    public Player create(Player player) {
+
+        if (player.getId() != null) {
+            log.warn("Trying to create a Player with assigned ID: {}", player.getId());
+            return null;
+        }
+
+        // guardar el jugador recibido por parámetro en la base de datos
+        Player result = repository.save(player);
+        log.info("Player created with ID: {}", player.getId());
+
+        return result;
+    }
+
+    public Player update(Player player) {
+
+        if (player.getId() != null && !repository.existsById(player.getId())) {
+            log.warn("Trying to update a Player with wrong ID");
+            return null;
+        }
+
+        Player result = repository.save(player);
+        log.info("Player updated with ID: {}", player.getId());
+
+        return result;
+    }
+
+    public Boolean delete(Long id) {
+
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            log.info("Player deleted with ID: {}", id);
+            return true;
+        }
+
+        log.warn("Trying to delete a Player with wrong ID");
+        return false;
+    }
+
+    /*
+     TODO: Validate Entity
+        email
+     */
+}
---
+++ src/main/java/com/example/tareaspring/services/TeamServiceImp.java
@@ -0,0 +1,139 @@
+package com.example.tareaspring.services;
+
+import com.example.tareaspring.models.Team;
+import com.example.tareaspring.csv.TeamCSV;
+import com.example.tareaspring.repositories.TeamRepository;
+import com.opencsv.bean.BeanVerifier;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.exceptions.CsvConstraintViolationException;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+// TODO: Move to Lombok @log annotation
+@Service
+@Log4j2
+public class TeamService {
+    private final TeamRepository repository;
+
+    public TeamService(TeamRepository repository) {
+        this.repository = repository;
+    }
+
+    // TODO: Refactorize
+    public List<Team> parseCSVFileToTeams(@NonNull MultipartFile file) {
+
+        List<Team> teams = new ArrayList<>();
+
+        // validamos que el fichero existe
+        if (file.isEmpty()) {
+            log.warn("Not CSV file present to upload");
+            return teams;
+        }
+
+        // parsemos el fichero CSV a una lista de objetos Team
+        try (Reader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+            CsvToBean<TeamCSV> csvToBean = new CsvToBeanBuilder(reader)
+                    .withType(TeamCSV.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .withVerifier(new BeanVerifier() {
+                        @Override
+                        public boolean verifyBean(Object bean) throws CsvConstraintViolationException {
+                            TeamCSV b = (TeamCSV) bean;
+                            // Todas propiedades nulas
+                            if (b == null) {
+                                return false;
+                            }
+                            return true;
+                        }
+                    })
+                    .build();
+
+            List<TeamCSV> teamsObj = csvToBean.parse();
+
+            for (TeamCSV o : teamsObj) {
+                Team team = o.toBeanWithId();
+                try {
+                    repository.save(team);
+                    teams.add(team);
+
+                } catch (Exception ex) {
+                    log.error("{} can't be saved: {}", team, ex.getMessage());
+                }
+            }
+
+        } catch (Exception ex) {
+            log.error("An error occurred while processing the CSV file.");
+        }
+
+        return teams;
+    }
+
+    public List<Team> findAll() {
+        log.info("Retrieving all Teams from database");
+        return repository.findAll();
+    }
+
+    public Team findById(Long id) {
+        Optional<Team> teamOpt = repository.findById(id);
+
+        if (teamOpt.isPresent()) {
+            log.info("Retrieving Team with ID: {}", id);
+            return teamOpt.get();
+        }
+        log.warn("Retrieving Team with wrong ID: {}", id);
+        return null;
+    }
+
+    public Team create(Team team) {
+
+        if (team.getId() != null) {
+            log.warn("Trying to create a Team with assigned ID: {}", team.getId());
+            return null;
+        }
+
+        Team result = repository.save(team);
+        log.info("Team created with ID: {}", team.getId());
+
+        return result;
+    }
+
+    public Team update(Team team) {
+
+        if (team.getId() != null && !repository.existsById(team.getId())) {
+            log.warn("Trying to update a Team with wrong ID");
+            return null;
+        }
+
+        Team result = repository.save(team);
+        log.info("Team updated with ID: {}", team.getId());
+
+        return result;
+    }
+
+    public Boolean delete(Long id) {
+
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            log.info("Team deleted with ID: {}", id);
+            return true;
+        }
+
+        log.warn("Trying to delete a Team with wrong ID");
+        return false;
+    }
+
+    /*
+     TODO: Validate Entity
+        email
+     */
+}
