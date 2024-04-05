9ac7f7f3b2d4c615b519bce3a817f75171675eef
Jucavi <jcarvil@gmail.com>
2024-04-06 00:51:57 +0200

    add custom error handler

--- src/main/java/com/example/tareaspring/errors/GlobalExceptionHandler.java
+++ src/main/java/com/example/tareaspring/errors/GlobalExceptionHandler.java
@@ -1,49 +1,53 @@
 package com.example.tareaspring.errors;
 
-import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.util.*;
+
 
 @Log4j2
 @RestControllerAdvice
-@NoArgsConstructor
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleMethodArgumentException(MethodArgumentNotValidException exception, WebRequest request) {
+    public ResponseEntity<Map<String, Object>> handleMethodArgumentException(MethodArgumentNotValidException ex, WebRequest request) {
 
-        log.error(exception.getMessage());
+        log.error(ex.getMessage());
 
         Map<String, String> errors = new HashMap<>();
-        exception.getBindingResult().getAllErrors().forEach(error -> {
+        Map<String, Object> body = new HashMap<>();
+
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.UNPROCESSABLE_ENTITY.value());
+
+        ex.getBindingResult().getAllErrors().forEach(error -> {
             String field = ((FieldError) error).getField();
             String message = error.getDefaultMessage();
             errors.put(field, message);
         });
 
-        return errors;
+        body.put("errors", errors);
+
+        return ResponseEntity.unprocessableEntity().body(body);
     }
 
-    @ExceptionHandler({HttpMessageNotReadableException.class, Exception.class})
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<ErrorResponse> handleUncaughtException(Exception exception, WebRequest request) {
+//    @ExceptionHandler(HttpMessageNotReadableException.class)
+//    public ResponseEntity<Map<String, String>> handleException(HttpMessageNotReadableException ex, WebRequest request) {
+//
+//        log.error(ex.getMessage());
+//
+//        Map<String, String> errors = new HashMap<>();
+//        errors.put("field ", ex.getMessage());
+//
+//        return ResponseEntity.unprocessableEntity().body(errors);
+//    }
 
-        log.error(exception.getMessage());
-
-        ErrorResponse error = new ErrorResponse();
-        error.setMessage(exception.getMessage());
-
-        return ResponseEntity.internalServerError().body(error);
-    }
 }
