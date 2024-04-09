0a29de778673319c340984d6a204d828e77912eb
jucavi <jcarvi@gmail.com>
2024-04-09 09:52:54 +0200

    change LocalDate to String, add validation to date ass csv annotations add builder

--- src/main/java/com/example/tareaspring/dto/TeamDto.java
+++ src/main/java/com/example/tareaspring/dto/TeamDto.java
@@ -1,34 +1,38 @@
 package com.example.tareaspring.dto;
 
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
+import com.example.tareaspring.utils.validators.anotations.CustomDateFormat;
+import com.opencsv.bean.CsvBindByName;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-import java.time.LocalDate;
 
 @Setter
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class TeamDto {
 
+    @CsvBindByName(required = true)
     private Long id;
 
+    @CsvBindByName(required = true)
     @NotBlank(message = "Team name can't be blank")
     private String name;
 
+    @CsvBindByName(required = true)
     @Pattern(regexp = "^[a-zA-Z0-9_!#$%&'*+=?`{|}~^.-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{1,4}$", message = "Email address must be valid")
     private String email;
 
+    @CsvBindByName(required = true)
     @NotNull
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private LocalDate since;
+    @CustomDateFormat(message = "Invalid date, pattern required YYYY-MM-DD")
+    private String since;
 
+    @CsvBindByName(required = true)
     @NotBlank(message = "City name can't be blank")
     private String city;
-
 }
