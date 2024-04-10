d5878c6159a48f1bfaf812b849f116063631b1b6
jucavi <jcarvi@gmail.com>
2024-04-10 11:15:18 +0200

    remove unused imports

--- src/main/java/com/example/tareaspring/TareaSpringApplication.java
+++ src/main/java/com/example/tareaspring/TareaSpringApplication.java
@@ -1,14 +1,9 @@
 package com.example.tareaspring;
 
 
-import com.example.tareaspring.services.PlayerService;
-import com.example.tareaspring.services.SigningService;
-import com.example.tareaspring.services.TeamService;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class TareaSpringApplication {
